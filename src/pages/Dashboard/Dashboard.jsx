c855aa4fb5e5afdf741faf36a9c2adf3172f3ef0
mkarche <karchemohamed@gmail.com>
2022-05-31 22:57:06 +0100

    Structuring the Page Contents

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,49 +1,82 @@
 import React, { useState } from "react";
 import { OverviewCard, SummaryCard, Tendency } from "../../components";
-import { Container } from "./Dashboard.styles";
+import {
+	Container,
+	Heading,
+	Line,
+	OverviewTitle,
+	OverviewWrapper,
+	Subtitle,
+	SummaryWrapper,
+	Switch,
+	SwitchButton,
+	SwitchText,
+	Title,
+} from "./Dashboard.styles";
 
 function Dashboard() {
 	const [darkMode, setDarkMode] = useState(0);
 	//console.log();
 	return (
-		<Container>
-			Dashboard
-			<Tendency value="-4" unit="%" />
-			<OverviewCard
-				darkMode={darkMode}
-				socialIcon="instagram"
-				title="Page views"
-				number={87}
-				value={3}
-				valueUnit="%"
-			/>
-			<OverviewCard
-				darkMode={1}
-				socialIcon="youtube"
-				title="Page views"
-				number={87}
-				value={3}
-				valueUnit="%"
-			/>
-			<SummaryCard
-				darkMode={darkMode}
-				socialIcon="instagram"
-				username="@mkarche"
-				number={87}
-				numberUnit="followers"
-				value={3}
-				valueUnit="Today"
-			/>
-			<SummaryCard
-				darkMode={1}
-				socialIcon="facebook"
-				username="@mkarche"
-				number={87}
-				numberUnit="subscribers"
-				value={3}
-				valueUnit="Today"
-			/>
-		</Container>
+		<>
+			<Container>
+				<Heading>
+					<Title>Social Media Dashboard</Title>
+					<Subtitle>Total Followers: 23,004</Subtitle>
+				</Heading>
+				<Line />
+				<Switch>
+					<SwitchText>Dark Mode</SwitchText>
+					<SwitchButton></SwitchButton>
+				</Switch>
+				<SummaryWrapper>
+					<SummaryCard
+						darkMode={darkMode}
+						socialIcon="instagram"
+						username="@mkarche"
+						number={87}
+						numberUnit="followers"
+						value={3}
+						valueUnit="Today"
+					/>
+					<SummaryCard
+						darkMode={1}
+						socialIcon="facebook"
+						username="@mkarche"
+						number={87}
+						numberUnit="subscribers"
+						value={3}
+						valueUnit="Today"
+					/>
+				</SummaryWrapper>
+				<OverviewTitle>Overview - Today</OverviewTitle>
+				<OverviewWrapper>
+					<OverviewCard
+						darkMode={darkMode}
+						socialIcon="instagram"
+						title="Page views"
+						number={87}
+						value={3}
+						valueUnit="%"
+					/>
+					<OverviewCard
+						darkMode={1}
+						socialIcon="youtube"
+						title="Page views"
+						number={87}
+						value={3}
+						valueUnit="%"
+					/>
+				</OverviewWrapper>
+			</Container>
+			<div class="attribution">
+				Challenge by{" "}
+				<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+					Frontend Mentor
+				</a>
+				. Coded by <a href="#">Med Karche</a>.
+			</div>
+		</>
 	);
 }
 
--- src/pages/Dashboard/Dashboard.styles.js
+++ src/pages/Dashboard/Dashboard.styles.js
@@ -7,6 +7,28 @@ export const Container = styled.div`
 	//align-items: center;
 `;
 
-export const Heading = styled.h1`
+export const Heading = styled.div``;
+
+export const Title = styled.h1`
 	margin: 0;
 `;
+
+export const Subtitle = styled.p`
+	margin: 0;
+`;
+
+export const Line = styled.hr``;
+
+export const Switch = styled.div``;
+
+export const SwitchText = styled.p``;
+
+export const SwitchButton = styled.button``;
+
+export const SummaryWrapper = styled.div``;
+
+export const OverviewTitle = styled.h1`
+	margin: 0;
+`;
+
+export const OverviewWrapper = styled.div``;
